d2e144b5b4702d8501322d4dac13422a535bc321
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-05-20 21:30:37 +0200

    tests added for the parsing of a correct and an incorrect file

--- tests/library/XMLParser/FileTest.php
+++ tests/library/XMLParser/FileTest.php
@@ -23,4 +23,30 @@ class XmlParser_FileTest extends Zend_Test_PHPUnit_ControllerTestCase
 		$file = XMLParser_File::getFile($name);
 		$this->assertTrue($file instanceof XMLParser_File);
 	}
+
+	public function testIncorrectFile()
+	{
+		$name = $this->bootstrap->getOption('storage_path') . '/data-bad-xml.xml';
+		$file = XMLParser_File::getFile($name);
+		try {
+			$file->parse();
+		} catch (Exception $e) {
+			$this->assertTrue($e instanceof XMLParser_Exception);
+		}
+
+		$this->assertTrue(is_null($file->getResult()));
+	}
+
+	public function testCorrectFile()
+	{
+		$name = $this->bootstrap->getOption('storage_path') . '/data.xml';
+		$file = XMLParser_File::getFile($name);
+		try {
+			$file->parse();
+			$this->assertTrue(true);
+		} catch (XMLParser_Exception $e) {
+			$this->assertTrue(false);
+		}
+		$this->assertTrue(is_array($file->getResult()));
+	}
 }
