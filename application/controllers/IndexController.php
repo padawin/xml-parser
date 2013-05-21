7ca7ff84dac00d8a1cc3177cf2abfb09e9105bd0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-05-21 16:45:12 +0200

    basic example in a web page

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('XMLParser/File/Sales.php');
+
 class IndexController extends Zend_Controller_Action
 {
 
@@ -10,7 +12,13 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+		// action body
+		$name = APPLICATION_PATH . '/../storage/data.xml';
+		$file = XMLParser_File_Sales::getFile($name);
+		//the rows will not be processed, neither displayed while parsed and
+		//will be kept in memory to be displayed after, in the view
+		$file->parse(0);
+		$this->view->data = $file->getResult();
     }
 
 
