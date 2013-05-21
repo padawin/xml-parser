2e30bc1155e77160b9da146e640cef15919d79e8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-05-21 22:23:59 +0200

    error message added

--- scripts/process-file.php
+++ scripts/process-file.php
@@ -7,7 +7,10 @@ include_once('XMLParser/File/Sales.php');
 
 $name = APPLICATION_PATH . '/../storage/data-huge.xml';
 $file = XMLParser_File_Sales::getFile($name);
-if (!empty($file)) {
+if (empty($file)) {
+	echo "The file $name was not found. Exiting.\n";
+}
+else {
 	//the rows will be displayed while parsed and not kept in memory
 	$file->parse(XMLParser_File::OPTION_RENDER | XMLParser_File::OPTION_EPHEMERAL);
 }
