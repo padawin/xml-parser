f41be1709c81242d0953627a36242192017b3b41
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-05-21 22:23:50 +0200

    file changed

--- scripts/process-file.php
+++ scripts/process-file.php
@@ -5,7 +5,7 @@ require('bootstrap.php');
 
 include_once('XMLParser/File/Sales.php');
 
-$name = APPLICATION_PATH . '/../storage/data-template.xml';
+$name = APPLICATION_PATH . '/../storage/data-huge.xml';
 $file = XMLParser_File_Sales::getFile($name);
 if (!empty($file)) {
 	//the rows will be displayed while parsed and not kept in memory
